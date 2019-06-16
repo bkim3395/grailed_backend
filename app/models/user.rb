cb76187ceeec61606a00262d14442cd9504f534c
bkim3395 <bkim3395@gmail.com>
2019-06-16 19:32:13 -0400

    Finalized the Project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
     validates :username, presence: true
 
-    # This class methods returns ActiveRecord:Relation of all users that have
+    # This class method returns ActiveRecord:Relation object of all users that have
     # forbidden usernames.
 
     # DisallowedUsername.all returns 7 rows of forbidden usernames.
-    # Number of User rows with forbidden usernames (before altering db): 25
+    # Number of User rows with forbidden usernames (before db alterations): 25
 
     def self.find_disallowed_usernames
         found_disallowed_users = User.where(username: DisallowedUsername
@@ -17,19 +17,19 @@ class User < ApplicationRecord
     # an incrementing integer to the username string. The first occurance of username
     # value will be ignored.
 
-    # It accepts one argument called "dry_run". Its default value is false. 
-    # If dry_run is false, then the function will alter the rows to resolve the 
-    # conflicts and update the database. If it is true, no alternation to the 
-    # database would be made.
+    # It accepts an argument called "dry_run". Its default value is false. 
+    # If it's false, then the function will alter the rows to resolve the 
+    # conflicts and update the database. If it's true, no alteration to the 
+    # rows and the database would be made and print the user row that have same usernames on console instead.
 
     # The function will return an ActiveRecord:Relations object that will
     # contain all User rows that have duplicate usernames with another User row.
-    # If dry_run was true, it will show updated usernames for each user row.
+    # If dry_run was false, it will show updated usernames for each User row.
 
     # This function ignores duplicate usernames that are disallowed because that
     # is handled by User.resolve_forbidden_usernames separately. 
 
-    # Number of User Rows that have duplicate usernames (before db alterncations): 681
+    # Number of User Rows that have duplicate allowed usernames (before db alterations): 681
 
     def self.resolve_duplicates(dry_run = false)
         counter = {};
@@ -64,22 +64,22 @@ class User < ApplicationRecord
     end
 
     # This class method resolves users that have forbidden usernames listed on
-    # disallowed_usernames table by concating an incrementing integer to the username.
+    # disallowed_usernames table by concating an incrementing integer to the username string.
     # By incrementing an integer, it also solves users that share same forbidden usernames.
     # The difference between this function and User.resolve_duplicates is that the first occurance
     # is not ignored and its username is concatinated with an integer so that no user has 
     # disallowed usernames.   
 
-    # Like User.resolve_duplicates, it accepts one argument called "dry_run".
+    # Like User.resolve_duplicates, it accepts an argument called "dry_run".
     # If dry_run is false, then the function will alter the rows to resolve the 
-    # conflicts and update the database. If it is true, no alternation to the 
-    # database would be made.
+    # conflicts and update the database. If it is true, no alteration to the 
+    # rows and the database would be made and print the user row that have forbidden usernames on console instead.
 
     # The function will return an ActiveRecord:Relations object that will
     # contain all User rows that have disallowed usernames.
-    # If dry_run was true, it will show updated usernames for each user row.
+    # If dry_run was false, it will show updated usernames for each User row.
 
-    # Number of User Rows that have duplicate usernames (before db alterncations): 25
+    # Number of User Rows that have forbidden usernames (before db alterations): 25
 
     def self.resolve_forbidden_usernames(dry_run = false)
         
@@ -122,7 +122,7 @@ class User < ApplicationRecord
 
     # This function resolves both duplicate usernames and disallowed usernames for all users.
     # It will return true if all conflicts has been successfully resolved. It will return
-    # false otherwise
+    # false otherwise.
 
     def self.resolve_all
         self.resolve_duplicates;
